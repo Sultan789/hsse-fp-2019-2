c321e5771186fdc42ad264c11155685e382b744c
Abdyraliev Sultanbek <suliphant@mail.ru>
2020-03-16 04:16:23 +0300

    1 лаб. работа

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,34 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
+    if (c > r) {
+      throw new IllegalArgumentException("Column must be fewer than row");
+    }
 
+    if (c == 0 || c == r) {
+      return 1
+    }
+
+    pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def check(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty) {
+        count == 0
+      } else if (chars.head == '(') {
+        check(chars.tail, count + 1)
+      } else if (chars.head == ')') {
+        (count > 0) && check(chars.tail, count - 1)
+      } else {
+        check(chars.tail, count)
+      }
+    }
+
+    check(chars, 0)
   }
 
   /**
@@ -33,6 +53,13 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) {
+      1;
+    } else if ((money < 0) || coins.isEmpty) {
+      0;
+    } else {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
 
   }
 }
